e8d703c0e73e3fbfd9bd6f62f1c078738191de04
Yaro <yarox24@users.noreply.github.com>
2016-05-13 22:22:27 +0200

    v 0.3
    - Added recursive option
    - Added option to suppress errors
    - Added lower/upper limits filter

--- binent.py
+++ binent.py
@@ -1,30 +1,44 @@
+#!/usr/bin/env python
 import argparse
-import os
-import sys
-import platform
 import math
-
-# TODO: REVISE ALL CODE
-# TODO: REVISE ALL CODE
-# TODO: REVISE ALL CODE
-# TODO: REVISE ALL CODE
+import os
+import platform
+import sys
 
 try:
    import numpy
 except:
-   print "Numpy library not detected. To fix this: "
+   print "Numpy library not installed. To fix this: "
    print "pip install numpy"
    sys.exit(-2)
 
-VERSION = "0.2 (beta)"
+VERSION = "0.3 (beta)"
 PROJECT_SITE = "https://github.com/yarox24/binent"
 
+
 # PARSER
+def entropy_float(x):
+   x = float(x)
+   if x < 0.0 or x > 8.0:
+      raise argparse.ArgumentTypeError("{:} - Entropy valid range is between 1.0-8.0   e.g. 7.993".format(x))
+   return x
+
+
+def utf_variable(string):
+   utf = string.decode(sys.getfilesystemencoding(), "replace")
+   return utf
+
+
 parser = argparse.ArgumentParser(description='Calculate entropy of a file(s)')
-parser.add_argument('-c', '--chunk', nargs=1, help='Fixed chunk size (e.g. 44K, 20M)')
-parser.add_argument('-r', '--recursive', nargs=1, help='Recursive TOP-BOTTOM traversal')
-parser.add_argument('sources', nargs='*', action='append', help="file1 file2 non_empty_dir/ [...]")
-args = parser.parse_args()
+parser.add_argument('-c', '--chunk', nargs=1, help='Fixed block-size (e.g. 256, 15K, 20M, 30G) Default: whole file')
+parser.add_argument('-r', '--recursive', dest='recursive', action='store_true', help='Recursive traversal')
+parser.add_argument('-l', "--low_limit", default=1.0, type=entropy_float,
+                    help='Show only entries with entropy >= limit (Default: 1.0)')
+parser.add_argument('-u', "--upper_limit", default=8.0, type=entropy_float,
+                    help='Show only entries with entropy <= limit (Default: 8.0)')
+parser.add_argument('-e', "--suppress_errors", action='store_true',
+                    help="Don't show any exceptions/errors and also empty files")
+parser.add_argument('sources', nargs='*', action='append', type=utf_variable, help="file1 file2 non_empty_dir/ [...]")
 
 
 def human2bytes(s):
@@ -76,24 +90,34 @@ def correct_float(fl):
    out = numpy.empty_like(fl, dtype=numpy.float64)
 
    if fl < 7.99:
-      numpy.round(fl, 3, out)
+      numpy.round(fl, 4, out)
    else:
-      # Eight is special case and shouldn't be rounded, just truncation 7.99xxxx = 7.99 and 8.0 = 8.0
+      # Eight is special case and shouldn't be rounded, just truncation 7.99xxxx = 7.99 and only real 8.0 = 8.0
       temp = numpy.empty_like(fl, dtype=numpy.float64)
       numpy.subtract(fl, numpy.round(fl - 0.005, 2, temp), temp)
       numpy.subtract(fl, temp, out)
-
    return out.item(0)
 
 
 def shortcut_filename(filename):
-   if len(filename) > 25:
-      cut = len(filename) - 25
+   if len(filename) > 40:
+      cut = len(filename) - 40
       center = len(filename) / 2
-      temp = filename[:center-1-int(math.ceil(cut/2)+1)] + "[..]" + filename[center+3+int(math.floor(cut/2)):]
+      temp = filename[:center - 1 - int(math.ceil(cut / 2) + 1)] + "[..]" + filename[
+                                                                            center + 3 + int(math.floor(cut / 2)):]
       return temp
    else:
       return filename
+
+
+def create_visual(float_entropy):
+   how_many = int(numpy.round(float_entropy, decimals=0))
+   return "|" + str("#" * how_many).ljust(8) + "|"
+
+
+def print_error(text, suppress_errors_flag):
+   if not suppress_errors_flag:
+      sys.stderr.write(text + "\n")
 
 
 def only_file_output(path, chunk_size):
@@ -109,43 +133,59 @@ def only_file_output(path, chunk_size):
          max_len = len(str(result['entropy_chunks_info'][-1][1])) + 2
          print "Chunks (Decimal offsets):"
          print ' ' + 'START'.center(max_len) + "STOP".center(max_len + 2) + ' ' + "ENTROPY".center(
-            10) + "VISUAL".center(10)
+            10) + " VISUAL".center(12)
 
          for i in range(0, count_chunks):
-            hashes = "|" + str(numpy.round(result['entropy_chunks'][i], decimals=0) * "#").ljust(8) + "|"
             print ' {:} {:} {:}  {:10}'.format(
                str(result['entropy_chunks_info'][i][0]).center(max_len),
                str(result['entropy_chunks_info'][i][1]).center(max_len + 1),
                " {: <9}".format(correct_float(result['entropy_chunks'][i])),
-               hashes)
+               create_visual(result['entropy_chunks'][i]))
    else:
-      print result['exception']
+      print_error(result['exception'], False)
       sys.exit(-3)
 
 
-def many_files_output(path, recursive):
-   if not hasattr(many_files_output, "i"):
-      many_files_output.i = 0
-      print 'FILENAME'.center(23) + "ENTROPY".center(13) + "       VISUAL"
+def many_files_output(path, comp, suppress_err):
+   # Unicode assumed
+   result = entropy_file(path, 0)
 
-   if many_files_output.i > 0:
-      result = entropy_file(path, 0)
+   filename = shortcut_filename(os.path.basename(path))
+   if result['exception'] == "":
+      # Filter results
+      if not comp(result['entropy']):
+         return
+      print ' {:<40} {:<13} {:<10}'.format(filename.encode("ascii", "replace"), correct_float(result['entropy']),
+                                           create_visual(result['entropy']))
+   else:
+      print_error(' {:<40} {:<13}'.format(filename.encode("ascii", "replace"), result['exception'].strip()),
+                  suppress_err)
 
-      filename = shortcut_filename(os.path.basename(path))
-      if result['exception'] == "":
-         entropy = correct_float(result['entropy'])
-         hashes = "|" + str(int(numpy.round(result['entropy'], decimals=0)) * "#").ljust(8) + "|"
-      else:
-         entropy = result['exception'].strip()
-         hashes = ""
 
-      print ' {:<25} {:<13} {:<10}'.format(filename, entropy, hashes)
+def recursive_output(starting_point, comp, suppress_err):
+   for (current_dir, _, filenames) in os.walk(starting_point):
+      for file in filenames:
+         absolute_path = os.path.join(current_dir, file)
+         if not os.path.isfile(absolute_path):
+            # print_error(absolute_path + ": Is not a file")
+            continue
 
-   many_files_output.i += 1
+         result = entropy_file(absolute_path, 0)
+         if result['exception'] != "":
+            print_error(absolute_path + ": " + result['exception'], suppress_err)
+         else:
+            # Filter results
+            if not comp(result['entropy']):
+               continue
+            print '"{:}",{:},{:},"{:}","{:}"'.format(absolute_path.encode("ascii", "replace"),
+                                                     correct_float(result['entropy']), result['entropy'],
+                                                     result['interpretation'], create_visual(result['entropy']))
 
 
 def interpretation(entropy):
-   if entropy >= 1.0 and entropy < 3.9:
+   if entropy == 0.0:
+      return "No information!"
+   elif entropy > 0.0 and entropy < 3.9:
       return "Low entropy. Potential for compression"
    elif entropy < 4.9:
       return "Possible plain text"
@@ -155,11 +195,11 @@ def interpretation(entropy):
       return "Poor compression/encryption"
    elif entropy < 7.00:
       return "Almost good compresion/encryption/randomness"
-   elif entropy < 7.50:
+   elif entropy < 7.70:
       return "Good compresion/encryption/randomness"
-   elif entropy < 7.99:
+   elif entropy < 8:
       return "Very good compresion/encryption/randomness"
-   elif entropy <= 8.00:
+   elif entropy == 8.00:
       return "ULTIMATE compresion/encryption/randomness!"
    else:
       return "Exception: Entropy out of range"
@@ -178,7 +218,6 @@ def provide_next(start, stop, max, one_time_max):
    return (start, stop, still_valid)
 
 
-# @profile
 def entropy_file(path, chunk_size):
    result = dict()
    result['entropy'] = 0.0
@@ -199,17 +238,17 @@ def entropy_file(path, chunk_size):
          result['exception'] = "Exception: Empty file"
          return result
 
-      if size > min(sys.maxsize - 100 * 1024 * 1024, 1024*1024*1024) and platform.architecture()[0] == "32bit":
+      if size > min(sys.maxsize - 100 * 1024 * 1024, 1024 * 1024 * 1024) and platform.architecture()[0] == "32bit":
          result['exception'] = "32-bit limit (1-2GB) use 64-bit python"
          return result
 
       mm = numpy.memmap(path, dtype='uint8', mode='r')
       while pointer < size:
-         fix_last_one = False
+         # fix_last_one = False
          # CHECK BOUNDARIES
          end = pointer + chunk_size
          if end > size:
-            fix_last_one = True
+            # fix_last_one = True
             end = size
          current_capacity = end - pointer
 
@@ -223,64 +262,74 @@ def entropy_file(path, chunk_size):
          if temp_end > end:
             temp_end = end
 
-         # print "Initial temp start:" + str((temp_start, temp_end))
          if current_capacity > MAX_ONE_TIME:
             while True:
-               # print "Optimized model: " + str((temp_start,temp_end))
                weights = numpy.add(numpy.bincount(mm[temp_start:temp_end], minlength=256), weights)
                (temp_start, temp_end, valid) = provide_next(temp_start, temp_end, end, MAX_ONE_TIME)
                if valid == False:
-                  # print "Model end"
                   break
          else:
-            # print "Normal model"
             weights = numpy.bincount(mm[pointer:end], minlength=256)
 
          prob = numpy.divide(weights, float(current_capacity))
-         tempopy = 0.0
+         result_temp = 0.0
 
          for i in range(0, 256):
             if prob[i] > 0.0:
-               tempopy += -prob[i] * numpy.log2(prob[i])
+               result_temp += -prob[i] * numpy.log2(prob[i])
          # if fix_last_one:
          #   tempopy = tempopy /numpy.log2(current_capacity) * numpy.log2(chunk_size)
-         result['entropy_chunks'].append(tempopy)
+         result['entropy_chunks'].append(result_temp)
          result['entropy_chunks_info'].append((pointer, end - 1))
          pointer += chunk_size
       result['entropy'] = numpy.mean(result['entropy_chunks'])
       result['interpretation'] = interpretation(result['entropy'])
-      # INTERPRETATION
-      try:
-         del mm
-      except NameError:
-         pass
 
    except Exception as e:
       result['exception'] = "Exception: " + e.message + e.__str__()
-      try:
-         del mm
-      except NameError:
-         pass
 
+   # CLEAN MEMORY MAPPING
+   try:
+      del mm
+   except NameError:
+      pass
    return result
 
 
 def main():
+   args = parser.parse_args()
+   recursive = args.recursive
    chunk_size = 0  # AUTOMATIC
    sources = args.sources[0]
+   low_limit = args.low_limit - 0.001
+   upper_limit = args.upper_limit + 0.001
+   suppress_errors = args.suppress_errors
+
+   if low_limit > upper_limit:
+      print "Error: Wrong limits. Nothing will be found"
+      sys.exit(-65)
+
+   entropy_filter = lambda x: True if x >= low_limit and x <= upper_limit else False
    items_number = len(sources)
 
    if items_number == 0:
+      print("binent v " + VERSION + "\t-== " + PROJECT_SITE + " ==-")
       print("*** Please provide at least one file or directory")
       print("")
       print("Examples:")
-      # TODO - REVISE!!!!!!!!!!!!!!!!!!!!!!!!
-      # print("1. Fix in-place 2 files (Make sure you got a copy!):")
-      # print(" " + sys.argv[0] + " AppEvent.Evt SysEvent.Evt")
-      # print("")
-      # print("2. Find all *.evt files in evt_dir/, copy them to fixed_copy/ and repair them:")
-      # print(" " + sys.argv[0] + " --copy_to_dir=fixed_copy evt_dir")
-      sys.exit(-10)
+      print("1. Calculate/interpret entropy of single file:")
+      print(" " + sys.argv[0] + " unk.zip")
+      print("")
+      print("2. Calculate entropy of single file with custom defined block-size = 10M(B):")
+      print(" " + sys.argv[0] + " -c 10M large.iso")
+      print("")
+      print("3. Calculate entropy of files inside directory (non-recursive):")
+      print(" " + sys.argv[0] + " mp3_dir")
+      print("")
+      print("4. Calculate entropy for files in directory and sub-directories (recursive):")
+      print(" " + sys.argv[0] + " -r starting_directory")
+      print("")
+      sys.exit(0)
 
    if args.chunk:
       test_size = human2bytes(args.chunk[0])
@@ -291,21 +340,27 @@ def main():
          sys.exit(-11)
 
    for path in sources:
-      path = path.rstrip("/\\").rstrip('"').rstrip("'")
+      if len(path) > 2:
+         path = path.rstrip("/\\").rstrip('"').rstrip("'")
       if os.path.isdir(path):
-         # NON-RECURSIVE
-         try:
-            files = os.listdir(path)
-
-         except Exception as e:
-            print("Error when listing directory: %s" % (path) + " [" + e.strerror + e.message + "]")
-            continue
-         for file in files:
-            full_path = path + os.path.sep + file
-            if os.path.isfile(full_path):
-               if not hasattr(many_files_output, "i"):
-                  many_files_output(full_path, False)
-               many_files_output(full_path, False)
+         if recursive:
+            print 'Absolute_path,Entropy_rounded,Entropy_precise,Interpretation,Visual'
+            recursive_output(path, entropy_filter, suppress_errors)
+            break
+         else:
+            print 'FILENAME'.center(38) + "ENTROPY".center(15) + "     VISUAL"
+            # NON-RECURSIVE
+            # STILL UNICODE PROBLEMS WHEN PASSING DIR ON WINDOWS ON SOME CHARACTERS EXAMPLE: e with dash
+            try:
+               files = os.listdir(path)
+            except Exception as e:
+               if not suppress_errors:
+                  print("Error when listing directory: %s" % (path) + " [" + e.strerror + e.message + "]")
+               continue
+            for file in files:
+               full_path = path + os.path.sep + file
+               if os.path.isfile(full_path):
+                  many_files_output(full_path, entropy_filter, suppress_errors)
       elif os.path.isfile(path):
          only_file_output(path, chunk_size)
       else:
